719008435b156e70ba3e35f20793339f85462ca1
David <dg615@cam.ac.uk>
2023-01-25 10:56:01 +0000

    modified main1

--- Arduino/main1/main1.ino
+++ Arduino/main1/main1.ino
@@ -2,43 +2,45 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+#define close_ls_left 13 // Left line sensor pin number
+#define close_ls_right 12 // Right line sensor pin number
+
+// Create motor objects
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *right_motor = AFMS.getMotor(1);
 Adafruit_DCMotor *left_motor = AFMS.getMotor(2);
-bool going = true;
+
+// Useful flags
+bool going;
 bool block_detected = false;
+short red_sensor, blue_sensor;
 
 void setup() {
+
+ Serial.begin(9600);
  AFMS.begin();
  left_motor->setSpeed(150);
  right_motor->setSpeed(150);
- pinMode(13, INPUT);
- pinMode(12, INPUT);
-
- pinMode(10, INPUT);
- pinMode(9, INPUT);
- Serial.begin(9600);
-
+ left_motor->run(FORWARD);
+ right_motor->run(FORWARD);
+ going = true;
+ pinMode(close_ls_left, OUTPUT);
+ pinMode(close_ls_right, OUTPUT);
 }
 
 // have a way to read from ultrasound
+float distance_from_block (){
+  
+}
 
 void loop() {
   int left_sensor = digitalRead(13);
   int right_sensor = digitalRead(12);
- 
-  int red_sensor = digitalRead(10);
-  int blue_sensor = digitalRead(9);
+
   bool turn_right;
   bool turn_left;
-
-
-
-
-  float distance_from_block (){
-    
-  }
-
+  bool go_straight;
+  bool in_tunnel;
    
   // assume 1 is white
   if(right_sensor == 0 && left_sensor == 0) {
@@ -56,32 +58,30 @@ void loop() {
     turn_left = false;
     turn_right = true;
   }
-  else {
+  else if (in_tunnel) {
     // in tunnel or sommething wrong if in tunnel sound sensor
     
   }
 
-  if ( going == true){
-    if ( go_straight == true) {
-      left_motor->run(FORWARD)
-      right_motor->run(FORWARD)
+  if (going == true){
+    if (go_straight == true) {
+      // do nothing
     }
-    if ( turn_left == true) {
+    if (turn_left == true) {
       left_motor->setSpeed(100);
       right_motor->setSpeed(80);
     }
-    if ( turn_left == true) {
+    if (turn_left == true) {
       left_motor->setSpeed(80);
       right_motor->setSpeed(100);
     }
   }
 
-  if ( red_sensor == 1 || blue_sensor == 1){
+  if (red_sensor == 1 || blue_sensor == 1){
     going == false;
     block_detected == true;
   }
 
-  if
 
   
   
--- Arduino/sketch_jan23a/sketch_jan23a.ino
+++ Arduino/sketch_jan23a/sketch_jan23a.ino
@@ -3,15 +3,14 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor = AFMS.getMotor(2);
 
 void setup() {
 Serial.begin (9600);
 AFMS.begin();
-myMotor->setSpeed(150);
-myMotor->run(FORWARD);
+
 }
 
 void loop() {
-  
-}
+
+} 
